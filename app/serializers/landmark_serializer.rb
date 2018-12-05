2f09c5ca154cd9afda32122d8ba2122455078f43
Chris Stull <chris.stull@gmail.com>
2018-12-05 11:23:14 -0500

    Corrected landmark serializer

--- app/serializers/landmark_serializer.rb
+++ app/serializers/landmark_serializer.rb
@@ -1,6 +1,6 @@
 class LandmarkSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes(:details,:user_id,:trail_id,:coords,:image_url)
+  attributes(:id,:details,:user_id,:trail_id,:coords,:image_url)
 
   def image_url
     return rails_blob_url(object.image)
