bc9d756376a93514a4e1d044677744fe3c34d230
Chris Stull <chris.stull@gmail.com>
2019-01-10 15:42:29 -0500

    Switched serializer for User avatars

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,7 +3,11 @@ class UserSerializer < ActiveModel::Serializer
   attributes(:id,:name,:age,:proficiency,:address,:coords,:avatar_url)
 
   def avatar_url
-     return rails_blob_url(object.avatar)
+    # local storage url
+    # return rails_blob_url(object.avatar)
+
+    # AWS url
+    return url_for(object.avatar)
   end
 
 end
